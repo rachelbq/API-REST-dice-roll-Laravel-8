11aba2c82620000673b27857e64a560c44ea917c
Rachel B <rbbeloved@gmail.com>
2023-02-05 13:46:10 +0100

    models & plays table creation & migration

---
+++ database/migrations/2023_02_05_121858_create_plays_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plays', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->nullable()
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+                    
+            $table->tinyInteger('dice1');
+            $table->tinyInteger('dice2');
+            $table->tinyInteger('sum');
+            $table->string('result');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plays');
+    }
+}
