832c16f89eef34387336a6b5643dd978207528ed
Rachel B <rbbeloved@gmail.com>
2023-02-05 13:15:54 +0100

    passport installation and environment configuration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nickname', 25)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'player']);
             $table->rememberToken();
             $table->timestamps();
         });
